c9c454d90f56b0c3faf0324bd175ebc110f6fd10
David Mkhitaryan <daveinc.mine@gmail.com>
2023-11-19 03:21:24 +0400

    Auth inputs value data is collecting

--- src/components/pages/Register.tsx
+++ src/components/pages/Register.tsx
@@ -1,25 +1,53 @@
-import React, { ComponentType } from 'react'
+import React, { ComponentType, useState } from 'react'
 import '../../style/Login.css'
 import { Link } from 'react-router-dom';
 import { RegInput } from '../ui-elements/RegInput';
 
+export enum fields {
+	'username',
+	'password',
+	'rePassword'
+}
 
 const passworWalidation = (username:string, password:string, rePassword:string) =>{
 
 }
 
-export const RegisterPage:ComponentType<{}> = () => (
-	<div>
-		<form style={{height: 610}}>
-			<h3>Register Now</h3>
+interface IUserinputData{
+	username:string
+	password:string,
+	rePassword:string
+}
+const validdata = /.{4,32}$/
 
-			
-			<RegInput name='Username'/>
-			<RegInput name='Password'/>
-			<RegInput name='Reenter Password'/>
-			<br />
-			<Link to="./Login"  className='underline'>Login</Link>
-			<button formMethod='post'>Register</button>
-		</form>
-	</div>
-)
+export const RegisterPage:ComponentType<{}> = () => {
+	const userinputData:IUserinputData = {
+		username: '',
+		password: '',
+		rePassword: ''
+	}
+	const handleValue = (field:number, event:any)=>{
+		if(field === fields.username)
+			userinputData.username = event.target.value
+		else if(field === fields.password)
+			userinputData.password = event.target.value
+		else if(field === fields.rePassword)
+			userinputData.rePassword = event.target.value
+		console.log(userinputData)
+	}
+	return(
+		<div>
+			<form style={{height: 610}}>
+				<h3>Register Now</h3>
+				<span className='errorLog'>Error message</span>			
+				<RegInput inputName='Username' handleValue={handleValue} />
+				<RegInput inputName='Password' handleValue={handleValue}/>
+				<RegInput inputName='RePassword' handleValue={handleValue} />
+				<br />
+				<Link to="./Login"  className='underline'>Login</Link>
+				<button formMethod='post'>Register</button>
+			</form>
+		</div>
+	
+	)
+}
--- src/components/ui-elements/RegInput.tsx
+++ src/components/ui-elements/RegInput.tsx
@@ -1,38 +1,38 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import '../../style/RegInput.css'
 
-const validdata = /.{4,32}$/
 
-export const RegInput = ({name}:{name:string})=>{
-    let is_password:boolean = false
-    if (name.search('Password') !== -1)
-    is_password = true
+interface IRegInput{
+    name:string,
+    retValue: string 
+}
+
+export const RegInput = ({inputName,handleValue}:{inputName:string, handleValue(field:number, event: any):any}) =>{
     const [show_pass, set_show_pass]= useState(true)
-    let [valid_value, set_valid_value] = useState(true)
-    function valueChange(event:any){
-        set_valid_value(()=>{
-            console.log(validdata.test(event.target.value))
-            return validdata.test(event.target.value)
-        })
 
+    let field:number = 0
+    switch(inputName)
+    {
+        case 'Username':
+            field = 0   
+            break;
+        case 'Password':
+            field = 1 
+            break  
+        case 'RePassword':
+            field = 2  
+            break 
     }
 
     return (
         <div className='main-div'>
-            <label htmlFor="password">{name}</label>
-            <input type={(is_password && show_pass) ? 'password' : 'text'} placeholder={name} id="password" className={is_password ? 'input-password' : 'input-text'} onChange={valueChange}/>
-                {
-                    is_password && 
-                    <img src='assets/images/icons/eye.png' style={{cursor: 'pointer'}} className='pass-eye' alt="" onClick={() => set_show_pass(!show_pass)} />
-                }
-                {
-                    valid_value === false &&
-                    <span style={{color: 'red',}} className={is_password ? 'password-span' : ''}>Minimum 4 character</span>
-                }
+            <label htmlFor="password">{inputName}</label>
+            <input type={(field > 0 && show_pass) ? 'password' : 'text'} placeholder={inputName} className={field > 0 ? 'input-password' : 'input-text'} onChange={(e) => handleValue(field, e)}/>
+            {
+                field > 0 && 
+                <img src='assets/images/icons/eye.png' style={{cursor: 'pointer'}} className='pass-eye' alt="" onClick={() => set_show_pass(!show_pass)} />
+            }
         </div>
     )
-}
-
-function useEfffect(arg0: () => void) {
-    throw new Error('Function not implemented.')
+        
 }
